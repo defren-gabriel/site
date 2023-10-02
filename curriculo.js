259d6b950946afbbbb7cb08dadfc2b3fa1fe4bc6
Gabriel Fernandes <defren.gabriel@gmail.com>
2023-10-02 14:11:54 -0300

    remoção da mudança de tema da pagina

--- curriculo.js
+++ curriculo.js
@@ -1,15 +1,5 @@
 function init(){
-    VerificaTema();
+    console.log('Pagina carregada com sucesso');
 };
-
-function VerificaTema() {
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        // O sistema operacional está usando o tema escuro
-        document.body.classList.add('tema-escuro');
-    } else {
-        // O sistema operacional está usando o tema claro
-        document.body.classList.add('tema-claro');
-    }
-}
 
 window.onload = init;
--- trabalhos.js
+++ trabalhos.js
@@ -1,28 +1,5 @@
 function init(){
-    VerificaTema();
+    console.log('Pagina carregada com sucesso');
 };
-
-function VerificaTema() {
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        // O sistema operacional está usando o tema escuro
-        document.body.classList.add('tema-escuro');
-    } else {
-        // O sistema operacional está usando o tema claro
-        document.body.classList.add('tema-claro');
-
-        
-        // Seleciona todos os elementos com a classe "link"
-        var elementosComClasseLink = document.querySelectorAll('.link');
-
-        // Itera sobre os elementos e altera a classe
-        elementosComClasseLink.forEach(function(elemento) {
-            // Remove a classe "link" do elemento
-            elemento.classList.remove('link');
-    
-            // Adiciona a classe "link-escuro" ao elemento
-            elemento.classList.add('link-escuro');
-        });
-    }
-}
 
 window.onload = init;
